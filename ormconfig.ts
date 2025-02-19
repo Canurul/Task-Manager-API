9024dd68f0bae39e32aea2c1b6b465e401acd46e
Artrediss <canurul@sabanciuniv.edu>
2025-02-19 12:14:09 +0300

    Server is built

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,14 +1,17 @@
 import { DataSource } from "typeorm";
 import { Task } from "./src/entities/Task";
 import { User } from "./src/entities/User";
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export const AppDataSource = new DataSource({
   type: "mysql",
   host: "localhost",
   port: 3306,
-  username: "user",
-  password: "userpassword",
-  database: "task_db",
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
   synchronize: true, //use only in development!!!
   logging: true,
   entities: [User, Task], 
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,21 @@
+import express from 'express';
 import { AppDataSource } from "../ormconfig";
+import dotenv from 'dotenv';
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
 
 AppDataSource.initialize()
 .then(() => console.log('Conntected to DB'))
-.catch((error)=> console.log('DB connection error', error))
+.catch((error)=> console.log('DB connection error', error));
+
+app.get('/', (req, res) => {
+    res.send('Task manager API is running');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on Port ${PORT}`)
+});
